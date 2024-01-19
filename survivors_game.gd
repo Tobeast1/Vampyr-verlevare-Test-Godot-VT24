1ace99bec74e93a0a847a547a8ddbb63bd4a123b
Tobeast1 <156183133+Tobeast1@users.noreply.github.com>
2024-01-19 13:12:54 +0100

    Added Restart and exit buttons

--- survivors_game.gd
+++ survivors_game.gd
@@ -15,3 +15,14 @@ func _on_timer_timeout():
 func _on_player_health_depleted():
 	%GameOver.visible = true
 	get_tree().paused = true
+
+
+
+
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_exit_pressed():
+	get_tree().quit()
